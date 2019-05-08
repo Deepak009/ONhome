422beb96fe24826cca7a7fb6c7183a29d09204e2
dpkdhariwal@gmail.com <Drock@009>
2019-05-08 17:56:10 +0530

    comit by deepak 01.49am 7-5-2019

---
+++ app/src/main/java/tech/iwish/ONhome/ItemDescription.java
@@ -0,0 +1,69 @@
+package tech.iwish.ONhome;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+
+public class ItemDescription extends AppCompatActivity {
+TextView addtocart;
+    String stringImageUri;
+    int imagePosition;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_description);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        if (getIntent() != null) {
+
+            getIntent().getStringExtra("item_id");
+            getIntent().getStringExtra("price");
+            getIntent().getStringExtra("save_price");
+            getIntent().getStringExtra("description");
+            getIntent().getStringExtra("off_price");
+
+            Log.e("IDee",getIntent().getStringExtra("item_id"));
+
+        }
+
+        addtocart = (TextView) findViewById(R.id.text_action_bottom1);
+
+        addtocart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Toast.makeText(ItemDescription.this,"Item added to cart.",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+
+    public  boolean onOptionsItemSelected(MenuItem item){
+        int id =item.getItemId();
+
+        if(id == android.R.id.home){
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ app/src/main/java/tech/iwish/ONhome/adaptors/BestSellAdaptor.java
@@ -0,0 +1,119 @@
+package tech.iwish.ONhome.adaptors;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.provider.SyncStateContract;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import tech.iwish.ONhome.ItemDescription;
+import tech.iwish.ONhome.R;
+
+public class BestSellAdaptor extends RecyclerView.Adapter<BestSellAdaptor.MyView> {
+
+    private ArrayList<String> item_id;
+    private ArrayList<String> price;
+    private ArrayList<String> save_price;
+    private ArrayList<String> description;
+    private ArrayList<String> off_price;
+    Activity activity;
+
+    public class MyView extends RecyclerView.ViewHolder {
+        public TextView item_id;
+        public TextView price;
+        public TextView save_price;
+        public TextView description;
+        public TextView Off_price;
+        public ImageView item_img_view;
+
+        public MyView(View view) {
+            super(view);
+
+            price = (TextView) view.findViewById(R.id.textView3);
+            save_price = (TextView) view.findViewById(R.id.textView5);
+            description = (TextView) view.findViewById(R.id.textView6);
+            Off_price = (TextView) view.findViewById(R.id.textView4);
+            item_id = (TextView) view.findViewById(R.id.item_id);
+            item_img_view = (ImageView) view.findViewById(R.id.imageView3);
+        }
+    }
+
+    public BestSellAdaptor(FragmentActivity activity,
+                           ArrayList<String> item_id,
+                           ArrayList<String> price,
+                           ArrayList<String> save_price,
+                           ArrayList<String> description,
+                           ArrayList<String> off_price) {
+        this.activity = activity;
+        this.item_id = item_id;
+        this.price = price;
+        this.save_price = save_price;
+        this.description = description;
+        this.off_price = off_price;
+
+
+    }
+
+    @Override
+    public MyView onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.singleitem, parent, false);
+
+        return new MyView(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final MyView holder, final int position) {
+
+        // Log.e("name", String.valueOf(horizontalProductScrollModelList.get(position)));
+        holder.item_id.setText(item_id.get(position));
+        holder.price.setText(price.get(position));
+        holder.save_price.setText(save_price.get(position));
+        holder.description.setText(description.get(position));
+        holder.Off_price.setText(off_price.get(position));
+        holder.description.setText(description.get(position));
+        Picasso.with(this.activity)
+                .load("https://image.shutterstock.com/image-photo/green-shopping-bag-grocery-products-260nw-144819322.jpg")
+                .into(holder.item_img_view);
+
+        holder.item_img_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(activity,ItemDescription.class);
+
+                intent.putExtra("item_id",item_id.get(position));
+                intent.putExtra("price",price.get(position));
+                intent.putExtra("save_price",save_price.get(position));
+                intent.putExtra("description",description.get(position));
+                intent.putExtra("off_price",off_price.get(position));
+
+                holder.item_img_view.getContext().startActivity(intent);
+                //Toast.makeText(activity, "Item"+position, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        //holder.item_img.setImageResource(R.drawable.slide4);
+        //Log.e("TestURL", Constants.BaseUrl + category_url.get(position));
+        /*Picasso.with(activity).load(SyncStateContract.Constants.BaseUrl + category_url.get(position))
+                .into(holder.imageView);*/
+    }
+
+    @Override
+    public int getItemCount() {
+        return item_id.size();
+    }
+}
+
